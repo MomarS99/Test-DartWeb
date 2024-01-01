84527c4cacf1306a1d1eec016d1626acc2c41a67
MomarS99 <salhi.omar.m@gmail.com>
2024-01-01 20:04:41 +0100

    init

---
+++ lib/country.dart
@@ -0,0 +1,113 @@
+// ignore_for_file: unused_import, prefer_const_constructors, use_key_in_widget_constructors, library_private_types_in_public_api, sized_box_for_whitespace
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:cardapp/main.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late List data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('choose your city'),
+        ),
+        body: Center(
+          child: FutureBuilder(
+            future:
+                DefaultAssetBundle.of(context).loadString('assets/test.json'),
+            builder: (context, snapshot) {
+              // Decode the JSON
+              var newData = json.decode(snapshot.data.toString());
+
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 32, bottom: 32, left: 16, right: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (_) => Welcome()));
+                                },
+                                child: Text(
+                                  newData[index]['city'],
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 22),
+                                ),
+                              ),
+                            ],
+                          ),
+                          //SizedBox(width: 20),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                itemCount: newData == null ? 0 : newData.length,
+              );
+            },
+          ),
+        ));
+  }
+}
+
+class Welcome extends StatefulWidget {
+  @override
+  _WelcomeState createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome'),
+      ),
+      body: Center(
+        child: Text(
+          "You are Welcome",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
